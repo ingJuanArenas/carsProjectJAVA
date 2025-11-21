58f1c548c4ced6ea603213c054c7eb64801e0ac0
ingJuanArenas <juanarenasing@gmail.com>
2025-11-21 15:58:09 -0500

    updating controller, now using response entity

--- src/main/java/com/cars/cars/web/controller/carsController.java
+++ src/main/java/com/cars/cars/web/controller/carsController.java
@@ -1,16 +1,47 @@
 package com.cars.cars.web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cars.cars.domain.dto.CarDTO;
+import com.cars.cars.domain.service.CarService;
+
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
+@RequestMapping("/cars")
 public class carsController {
-    
-    @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
-        return "Hello world!" + name.toUpperCase();
+
+    private final CarService carService;
+
+    public carsController(CarService carService) {
+        this.carService = carService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<CarDTO>> getAllCars() {
+        if (carService.getAllCars().isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(carService.getAllCars());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarDTO> getCarById(@PathVariable Long id) {
+
+        if (carService.getCarById(id) == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(carService.getCarById(id));
+    }
+    
+
     
 }
